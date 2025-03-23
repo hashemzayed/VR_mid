00f7e57f08af9f1b316088e760f1a09b0f72d939
hashemzayed <146767156+hashemzayed@users.noreply.github.com>
2025-03-23 22:41:09 +0200

    Core Implementation
    movement, turns, adding assets, grabbing, teleportation. all of what is in guides 1 through 5

---
+++ Midterm_VR/Assets/XRSocketTagInteractor2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSocketTagInteractor : XRSocketInteractor
+{
+    [Tooltip("Only objects with this tag can be snapped into this socket.")]
+    public string targetTag;
+
+    public override bool CanHover(XRBaseInteractable interactable)
+    {
+        // Only allow hover if the object has the correct tag.
+        return base.CanHover(interactable) &&
+               (interactable.transform.CompareTag(targetTag));
+    }
+
+    public override bool CanSelect(XRBaseInteractable interactable)
+    {
+        // Only allow selection (snapping) if the object has the correct tag.
+        return base.CanSelect(interactable) &&
+               (interactable.transform.CompareTag(targetTag));
+    }
+}
